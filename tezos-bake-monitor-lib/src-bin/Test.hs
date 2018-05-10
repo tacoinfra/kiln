a710b9f3ea8b51b5765890c70659d045522ae6c1
dbornside <dan.bornside@obsidian.systems>
2018-05-10 12:02:22 -0400

    Fix ProtoInfo instance to have most fields

--- tezos-bake-monitor-lib/src-bin/Test.hs
+++ tezos-bake-monitor-lib/src-bin/Test.hs
@@ -15,8 +15,11 @@ main :: IO ()
 main = do
   httpMgr <- liftIO $ newManager tlsManagerSettings
   let ctx = NodeRPCContext httpMgr "http://127.0.0.1:18731"
-  (flip runReaderT ctx $ doRPC $ Block $ BlockHash "head") >>= \case
-    RpcResponse_HttpException bad -> error $ show bad
-    RpcResponse_UnexpectedStatus bad -> error $ show bad
-    RpcResponse_NonJSON clue bad -> error $ (clue <> "\n" <> show bad)
-    RpcResponse_Success ok -> print ok
+  let step :: Show a => NodeRPCRequest a -> IO ()
+      step x = (flip runReaderT ctx $ doRPC $ x) >>= \case
+          RpcResponse_HttpException bad -> error $ show bad
+          RpcResponse_UnexpectedStatus bad -> error $ show bad
+          RpcResponse_NonJSON clue bad -> error $ (clue <> "\n" <> show bad)
+          RpcResponse_Success ok -> print ok
+  putStrLn "head" >> (step $ Block $ BlockHash "head")
+  putStrLn "constants" >> step ProtoConstants
