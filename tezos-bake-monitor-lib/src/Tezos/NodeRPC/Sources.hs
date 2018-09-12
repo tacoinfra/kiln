1d54cd58780dde901ebf7c4d9550782f335ba590
dbornside <dan.bornside@obsidian.systems>
2018-09-12 18:45:20 -0400

    add api to url

--- tezos-bake-monitor-lib/src/Tezos/NodeRPC/Sources.hs
+++ tezos-bake-monitor-lib/src/Tezos/NodeRPC/Sources.hs
@@ -59,8 +59,8 @@ canFetchHistory PublicNode_Obsidian = True
 
 
 getPublicNodeUri :: PublicNode -> NamedChain -> URI
-getPublicNodeUri PublicNode_Obsidian NamedChain_Zeronet  = [Uri.uri|https://tezos-api.obsidian.systems/zeronet|]
-getPublicNodeUri PublicNode_Obsidian NamedChain_Alphanet = [Uri.uri|https://tezos-api.obsidian.systems/alphanet|]
+getPublicNodeUri PublicNode_Obsidian NamedChain_Zeronet  = [Uri.uri|https://tezos-api.obsidian.systems/zeronet/api|]
+getPublicNodeUri PublicNode_Obsidian NamedChain_Alphanet = [Uri.uri|https://tezos-api.obsidian.systems/alphanet/api|]
 getPublicNodeUri PublicNode_Obsidian NamedChain_Betanet  = [Uri.uri|https://tezos-api.obsidian.systems/api|]
 getPublicNodeUri PublicNode_Blockscale NamedChain_Zeronet  = [Uri.uri|https://rpczero.tzbeta.net|]
 getPublicNodeUri PublicNode_Blockscale NamedChain_Alphanet = [Uri.uri|https://rpcalpha.tzbeta.net|]
