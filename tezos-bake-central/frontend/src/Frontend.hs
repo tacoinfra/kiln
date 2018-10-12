bb3c9101457816f79c86cec6a07e0aaedeebaa8d
dbornside <dan.bornside@obsidian.systems>
2018-10-12 18:48:00 -0400

    Partial work on styleguide

--- tezos-bake-central/frontend/src/Frontend.hs
+++ tezos-bake-central/frontend/src/Frontend.hs
@@ -748,15 +748,15 @@ mailServerForm frm0 = do
   where
     fields = withFormFieldsErr (frm0, "") $ do
       divClass "three fields" $ do
-        tellFieldErr (_1 . mailServerView_hostName) <=< formItem' "eight wide"
+        tellFieldErr (_1 . mailServerView_hostName) <=< formItem' "required eight wide"
           $ validatedInput Validator.validateText
           $ defTxt "Host" & Txt.setInitial (_mailServerView_hostName frm0)
 
-        tellFieldErr (_1 . mailServerView_portNumber) <=< formItem' "four wide"
+        tellFieldErr (_1 . mailServerView_portNumber) <=< formItem' "required four wide"
           $ validatedInput (Validator.validateNumeric "port" (Just 0, Just 65535) (Just 1))
           $ defTxt "Port" & Txt.setInitial (tshow $ _mailServerView_portNumber frm0)
 
-        tellFieldErr (_1 . mailServerView_smtpProtocol) <=< formItem' "four wide"
+        tellFieldErr (_1 . mailServerView_smtpProtocol) <=< formItem' "required four wide"
           $ fmap (fmap (maybe (Left "Please select a protocol") Right) . SemUi._dropdown_value)
           $ do
             labeled "Protocol"
