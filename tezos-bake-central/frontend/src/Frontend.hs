tacoinfra/kiln

commit 2a1b9e44b338dc9908e31507236ed6dda97b35ee

Make alert bell 40px tall, and center vertically

tezos-bake-central/frontend/src/Frontend.hs
@@ -362,7 +362,7 @@ headerBell = do
   alertCount <- holdUniqDyn =<< fmap (fromMaybe 0) <$> watchAlertCount
   (e,_) <- SemUi.ui' "span"
     (def
-      & SemUi.classes .~ (SemUi.Dyn $ ffor alertCount $ ((<>) "ui circular big label ") . bool "basic" "red link" . (>0))
+      & SemUi.classes .~ (SemUi.Dyn $ ffor alertCount $ ((<>) "ui circular label ") . bool "basic" "red link" . (>0))
       )
     $ do
         dynText $ ffor alertCount $ (fromMaybe <*> T.stripPrefix "0") . tshow

tezos-bake-central/static/semantic-ui/src/site/globals/site.overrides
@@ -279,6 +279,9 @@ body {
     .column {
       .ui.circular.label.link {
         cursor: pointer;
+        height: @40px;
+        font-size: 16px;
+        margin-top: 2px;
       }
     }
 

commit 6c5dc90a32251681d8d342bb2cc22d00e631382e

Fix alert bell UI in case of no error Fix for change introduced in 2a1b9e44b338dc9908e31507236ed6dda97b35ee

tezos-bake-central/frontend/src/Frontend.hs
@@ -362,7 +362,7 @@ headerBell = do
   alertCount <- holdUniqDyn =<< fmap (fromMaybe 0) <$> watchAlertCount
   (e,_) <- SemUi.ui' "span"
     (def
-      & SemUi.classes .~ (SemUi.Dyn $ ffor alertCount $ ((<>) "ui circular label ") . bool "basic" "red link" . (>0))
+      & SemUi.classes .~ (SemUi.Dyn $ ffor alertCount $ ((<>) "ui circular label link ") . bool "basic" "red" . (>0))
       )
     $ do
         dynText $ ffor alertCount $ (fromMaybe <*> T.stripPrefix "0") . tshow