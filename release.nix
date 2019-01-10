627c2181a185c4998bea67b171943a48b37ffc63
John Ericson <John.Ericson@Obsidian.Systems>
2019-01-10 17:00:42 -0500

    Get ready for Darwin jobs

--- release.nix
+++ release.nix
@@ -1,5 +1,13 @@
-let root = import ./. {}; in
-{
-  dockerExe = root.pkgs.lib.hydraJob root.dockerExe;
-  dockerImage = root.pkgs.lib.hydraJob root.dockerImage;
+let
+  perMachine = system: let
+   root = import ./. { inherit system; };
+  in {
+
+  } // root.pkgs.lib.optionalAttrs (system == "x86_64-linux") {
+    dockerExe = root.pkgs.lib.hydraJob root.dockerExe;
+    dockerImage = root.pkgs.lib.hydraJob root.dockerImage;
+  };
+in {
+  x86_64-linux = perMachine "x86_64-linux";
+  x86_64-darwin = perMachine "x86_64-darwin";
 }
