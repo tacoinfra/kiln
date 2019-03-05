2f8aeeb837759aee0d379b12d5567f1bbd847c79
Divam <dfordivam@gmail.com>
2019-03-05 11:37:36 +0900

    make Insufficient error a warning

--- tezos-bake-central/common/src/Common/Alerts.hs
+++ tezos-bake-central/common/src/Common/Alerts.hs
@@ -182,7 +182,7 @@ bakerInsufficientFundsDescriptions _{-elog-} = BakerErrorDescriptions
   , _bakerErrorDescriptions_tile = "Insufficient stake to receive rights."
   , _bakerErrorDescriptions_notification = "This baker’s staking balance is less than 1 roll and cannot receive any baking or endorsing rights."
   , _bakerErrorDescriptions_problem = "Bakers receive baking and endorsing rights based on the number of rolls (1 roll = 10,000ꜩ) in their staking balance (the baker’s balance plus any tez delegated to them). This baker’s staking balance is less than one roll and will not receive any baking or endorsing rights."
-  , _bakerErrorDescriptions_warning = Nothing
+  , _bakerErrorDescriptions_warning = Just ""
   , _bakerErrorDescriptions_fix = "Transfer tez or have other accounts delegate their tez to this baker so its staking balance is at least 1 roll."
   , _bakerErrorDescriptions_resolved = \_ ->
       ( "Resolved: Baker has sufficient funds to receive rights"
