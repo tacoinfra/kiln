5cf9eb656c068912e7712813512bc46a7b732057
Sebastien Mondet <seb@mondet.org>
2019-03-28 11:55:19 -0400

    Fix SQL query for deactivation risk

--- tezos-bake-central/backend/src/Backend/Alerts.hs
+++ tezos-bake-central/backend/src/Backend/Alerts.hs
@@ -207,7 +207,7 @@ reportBakerDeactivationRisk pkh gracePeriod latestCycle protoInfo newFit = do
     SELECT el.id, t.log, t.fitness
       FROM "ErrorLog" el
       JOIN "ErrorLogBakerDeactivationRisk" t ON t.log = el.id
-      JOIN "Baker" b ON b.publicKeyHash = t."publicKeyHash"
+      JOIN "Baker" b ON b."publicKeyHash" = t."publicKeyHash"
      WHERE NOT b."data#deleted"
        AND el.stopped IS NULL
      ORDER BY el."lastSeen" DESC, el.started DESC
