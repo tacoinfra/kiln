d23c4773331c0fe72e359764ffd7f53106f3e268
3noch <eacameron@gmail.com>
2019-04-10 08:07:45 -0400

    Add linux package upgrade instructions

--- tezos-bake-central/common/src/Common/Distribution.hs
+++ tezos-bake-central/common/src/Common/Distribution.hs
@@ -7,12 +7,15 @@ module Common.Distribution where
 data Distribution
   = Distribution_FromSource
   | Distribution_Docker
+  | Distribution_LinuxPackage
   deriving (Eq, Ord, Enum, Bounded, Read, Show)
 
 distributionMethod :: Distribution
 distributionMethod =
-#if defined(DOCKER)
+#if defined(DISTRO_DOCKER)
   Distribution_Docker
+#elif defined(DISTRO_LINUX_PACKAGE)
+  Distribution_LinuxPackage
 #else
   Distribution_FromSource
 #endif
