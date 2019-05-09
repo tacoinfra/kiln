6f96b5fe6d7623a712931bdab2356cd0bf69da38
Elliot Cameron <eacameron@gmail.com>
2019-05-09 16:58:28 -0400

    Teach Kiln about more protocols

--- tezos-bake-central/scoped-tzkits.nix
+++ tezos-bake-central/scoped-tzkits.nix
@@ -1,43 +1,18 @@
-{ nixpkgs ? import ((import <nixpkgs> {}).fetchFromGitHub {
-    owner = "nixos";
-    repo = "nixpkgs";
-    rev = "135a7f9604c482a27caa2a6fff32c6a11a4d9035";
-    sha256 = "0x5w33rv8y1zsmhxnac8d2jjp58w00zjhhg7m5pnswghcwgg6aqj";
-  }) {}
-}: let
-  obelisk = import (./.obelisk/impl) {};
-  reflex-platform = obelisk.reflex-platform;
-  nodeKit-src = import (reflex-platform.hackGet ../dep/tezos-baking-platform) {};
-in {
-  kits = nixpkgs.stdenv.mkDerivation {
-    name = "scoped-tzkits";
-    sourceRoot = ".";
-    # NO SOURCES
-    src = nixpkgs.runCommand "empty" {
-      outputHashMode = "recursive";
-      outputHashAlgo = "sha256";
-      outputHash = "0sjjj9z1dhilhpc8pq4154czrb79z9cm044jvn75kxcjv6v5l2m5";
-    } "mkdir $out";
-    configurePhase = "true";
-    installPhase = "true";
-    nativeBuildInputs = [];
-    buildInputs = [];
-    buildPhase = ''
-      mkdir -p $out/bin
-      for bin in $(ls ${nodeKit-src.tezos.mainnet.kit}/bin) ; do
-        ln -s ${nodeKit-src.tezos.mainnet.kit}/bin/$bin $out/bin/mainnet-$bin
-      done
-      for bin in $(ls ${nodeKit-src.tezos.alphanet.kit}/bin) ; do
-        ln -s ${nodeKit-src.tezos.alphanet.kit}/bin/$bin $out/bin/alphanet-$bin
-      done
-      for bin in $(ls ${nodeKit-src.tezos.zeronet.kit}/bin) ; do
-        ln -s ${nodeKit-src.tezos.zeronet.kit}/bin/$bin $out/bin/zeronet-$bin
-      done
-      for bin in $(ls ${nodeKit-src.tezos.master.kit}/bin) ; do
-        ln -s ${nodeKit-src.tezos.master.kit}/bin/$bin $out/bin/master-$bin
-      done
-
-      ls $out/bin
-    '';
-  };
-}
+{ pkgs, tezos-baking-platform }:
+let
+  tz = tezos-baking-platform.tezos;
+in pkgs.runCommand "scoped-tzkits" {} ''
+  mkdir -p $out/bin
+  for bin in $(ls ${tz.mainnet.kit}/bin) ; do
+    ln -s ${tz.mainnet.kit}/bin/$bin $out/bin/mainnet-$bin
+  done
+  for bin in $(ls ${tz.alphanet.kit}/bin) ; do
+    ln -s ${tz.alphanet.kit}/bin/$bin $out/bin/alphanet-$bin
+  done
+  for bin in $(ls ${tz.zeronet.kit}/bin) ; do
+    ln -s ${tz.zeronet.kit}/bin/$bin $out/bin/zeronet-$bin
+  done
+  for bin in $(ls ${tz.master.kit}/bin) ; do
+    ln -s ${tz.master.kit}/bin/$bin $out/bin/master-$bin
+  done
+''
