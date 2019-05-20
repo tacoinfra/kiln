229ba7ec4415c0f092f042fce794103c60625e36
Elliot Cameron <eacameron@gmail.com>
2019-05-20 14:45:27 -0400

    Nixify protocol test

--- default.nix
+++ default.nix
@@ -398,41 +398,53 @@ let
       --base-port=20000 --interactive=true --pause-on-error=true'
   '';
 
-  protocolTest = pkgs.writeScriptBin "protocol-test" ''
+  protocolTest = let
+    # hackGet = obelisk.reflex-platform.hackGet;
+    tezosBin = (import tests/dep/tezos-baking-platform {}).tezos.master.kit + /bin;
+    tezosClient = "${tezosBin}/tezos-client";
+    oldSuffix = "003-PsddFKi3";
+    newSuffix = "004-Pt24m4xi";
+    propto = (import tests/dep/tbp-multi-protocol-mainnet {}).tezos.mainnet.tezos-src + /src/proto_004_Pt24m4xi/lib_protocol;
+  in pkgs.writeScriptBin "protocol-test" ''
     #!/usr/bin/env bash
-    set -ve
-    echo 'Starting flextesa protocol test... monitor a node via kiln at http://127.0.0.1:20000'
+    set -Eeuo pipefail
 
-    export tezos_co=$PWD/dep/tezos-baking-platform/tezos/zeronet
-    export propto=$tezos_co/src/proto_004_Pt24m4xi/lib_protocol/
-    export old_suffix=003-PsddFKi3
-    export new_suffix=004-Pt24m4xi
-    export dpu_root_path=$PWD/dpu_root_path
-    export tezos_bin=$(nix-build -A tezos.zeronet.kit ./dep/tezos-baking-platform)/bin
-    export ledger="ledger://odd-himalayan-lustrous-falcon/ed25519/0'/0'"
-    export pkh=tz1NXDWqwMv1Zi7Jo9za7YN9orap94XQmFSv
+    fail() { "''${___fail:?$1}"; }
+    contains_re_group() { [[ $1 =~ $2 ]] && echo "''${BASH_REMATCH[1]}"; }
 
-    nix-shell -A tezos.master.sandbox dep/tezos-baking-platform --run \
-      'flextesa daemons-upgrade $propto \
-           --add-bootstrap LBK,edpkuSWMVjedhmQHarHMxvzdLV69cRWERM9yk4H8FAAfuexz3L9bCM,$pkh,$ledger@200_000_000_000 \
-           --no-daemons-for LBK \
-           --add-external 10000 \
-           --generate-kiln ./tezos-bake-central/config/,10000 \
-           --clean-kiln-config \
-           --time 4,4 \
-           --blocks-per-vot 40 \
-           --pause-on-error true \
-           --root-path $dpu_root_path \
-           --tezos-node-binary $tezos_bin/tezos-node \
-           --protocol-hash PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP \
-           --first-baker-alpha-binary        $tezos_bin/tezos-baker-$old_suffix \
-           --first-endorser-alpha-binary  $tezos_bin/tezos-endorser-$old_suffix \
-           --first-accuser-alpha-binary    $tezos_bin/tezos-accuser-$old_suffix \
-           --second-baker-alpha-binary       $tezos_bin/tezos-baker-$new_suffix \
-           --second-endorser-alpha-binary $tezos_bin/tezos-endorser-$new_suffix \
-           --second-accuser-alpha-binary   $tezos_bin/tezos-accuser-$new_suffix \
-           --tezos-client-binary $tezos_bin/tezos-client \
-           --tezos-admin-client-binary $tezos_bin/tezos-admin-client'
+    connected_ledgers=''$(${tezosClient} -P 0 list connected ledgers 2>/dev/null)
+    ledger_uri=$(contains_re_group "$connected_ledgers" '(ledger://[^\"]+)' || fail "Unable to find a connected ledger")
+    echo "> Ledger: $ledger_uri"
+
+    show_ledger=$(${tezosClient} -P 0 show ledger "$ledger_uri" 2>/dev/null)
+    pk=$(contains_re_group "$show_ledger" '\* Public Key: ([A-Za-z0-9]+)' || fail "Unable to determine public key for $ledger_uri")
+    echo "> PK: $pk"
+    pkh=$(contains_re_group "$show_ledger" '\* Public Key Hash: ([A-Za-z0-9]+)' || fail "Unable to determine public key hash for $ledger_uri")
+    echo "> PKH: $pkh"
+
+    echo 'Starting flextesa protocol test...'
+    echo '>>> Monitor a node via Kiln at http://127.0.0.1:20000 <<<'
+
+    ${tezosBin}/flextesa daemons-upgrade ${propto} \
+      --add-bootstrap "LBK,$pk,$pkh,$ledger_uri@200_000_000_000" \
+      --no-daemons-for LBK \
+      --add-external 10000 \
+      --generate-kiln ./tezos-bake-central/config/,10000 \
+      --clean-kiln-config \
+      --time 4,4 \
+      --blocks-per-vot 40 \
+      --pause-on-error true \
+      --root-path "$PWD/dpu_root_path" \
+      --tezos-node-binary ${tezosBin}/tezos-node \
+      --protocol-hash PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP \
+      --first-baker-alpha-binary     ${tezosBin}/tezos-baker-${oldSuffix} \
+      --first-endorser-alpha-binary  ${tezosBin}/tezos-endorser-${oldSuffix}} \
+      --first-accuser-alpha-binary   ${tezosBin}/tezos-accuser-${oldSuffix}} \
+      --second-baker-alpha-binary    ${tezosBin}/tezos-baker-${newSuffix} \
+      --second-endorser-alpha-binary ${tezosBin}/tezos-endorser-${newSuffix} \
+      --second-accuser-alpha-binary  ${tezosBin}/tezos-accuser-${newSuffix} \
+      --tezos-client-binary ${tezosClient} \
+      --tezos-admin-client-binary ${tezosBin}/tezos-admin-client
   '';
 
 in (obApp distroMethods.source) // {
