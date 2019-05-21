899a58f8eb73bfa7bc72e18cec7460f2d4697df6
Elliot Cameron <eacameron@gmail.com>
2019-05-21 14:20:08 -0400

    Move test scripts to their own folder

--- default.nix
+++ default.nix
@@ -385,70 +385,10 @@ let
     echo "'cd \"$KILN_INSTALL_PATH\"' and run './backend' to run kiln with default settings."
   '';
 
-  votingTest = pkgs.writeScriptBin "voting-test" ''
-    #!/usr/bin/env bash
-    set -e
-    echo 'Starting flextesa voting test... monitor a node via kiln at http://127.0.0.1:20000'
-    rm -rf /tmp/kiln_voting_test
-    cp -r ${(import ./dep/tezos-baking-platform {}).tezos.master.tezos-src}/src/bin_client/test/proto_test_injection /tmp/kiln_voting_test
-    chmod -R +w /tmp/kiln_voting_test
-    nix-shell -A tezos.master.sandbox dep/tezos-baking-platform --run \
-      'flextesa voting \
-      /tmp/kiln_voting_test \
-      --base-port=20000 --interactive=true --pause-on-error=true'
-  '';
-
-  protocolTest = let
-    # hackGet = obelisk.reflex-platform.hackGet;
-    tezosBin = (import tests/dep/tezos-baking-platform {}).tezos.master.kit + /bin;
-    tezosClient = "${tezosBin}/tezos-client";
-    oldSuffix = "003-PsddFKi3";
-    newSuffix = "004-Pt24m4xi";
-    propto = (import tests/dep/tbp-multi-protocol-mainnet {}).tezos.mainnet.tezos-src + /src/proto_004_Pt24m4xi/lib_protocol;
-  in pkgs.writeScriptBin "protocol-test" ''
-    #!/usr/bin/env bash
-    set -Eeuo pipefail
-
-    fail() { "''${___fail:?$1}"; }
-    contains_re_group() { [[ $1 =~ $2 ]] && echo "''${BASH_REMATCH[1]}"; }
-
-    connected_ledgers=''$(${tezosClient} -P 0 list connected ledgers 2>/dev/null)
-    ledger_uri=$(contains_re_group "$connected_ledgers" '(ledger://[^\"]+)' || fail "Unable to find a connected ledger")
-    echo "> Ledger: $ledger_uri"
-
-    show_ledger=$(${tezosClient} -P 0 show ledger "$ledger_uri" 2>/dev/null)
-    pk=$(contains_re_group "$show_ledger" '\* Public Key: ([A-Za-z0-9]+)' || fail "Unable to determine public key for $ledger_uri")
-    echo "> PK: $pk"
-    pkh=$(contains_re_group "$show_ledger" '\* Public Key Hash: ([A-Za-z0-9]+)' || fail "Unable to determine public key hash for $ledger_uri")
-    echo "> PKH: $pkh"
-
-    echo 'Starting flextesa protocol test...'
-    echo '>>> Monitor a node via Kiln at http://127.0.0.1:20000 <<<'
-
-    ${tezosBin}/flextesa daemons-upgrade ${propto} \
-      --add-bootstrap "LBK,$pk,$pkh,$ledger_uri@200_000_000_000" \
-      --no-daemons-for LBK \
-      --add-external 10000 \
-      --generate-kiln ./tezos-bake-central/config/,10000 \
-      --clean-kiln-config \
-      --time 4,4 \
-      --blocks-per-vot 40 \
-      --pause-on-error true \
-      --root-path "$PWD/dpu_root_path" \
-      --tezos-node-binary ${tezosBin}/tezos-node \
-      --protocol-hash PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP \
-      --first-baker-alpha-binary     ${tezosBin}/tezos-baker-${oldSuffix} \
-      --first-endorser-alpha-binary  ${tezosBin}/tezos-endorser-${oldSuffix}} \
-      --first-accuser-alpha-binary   ${tezosBin}/tezos-accuser-${oldSuffix}} \
-      --second-baker-alpha-binary    ${tezosBin}/tezos-baker-${newSuffix} \
-      --second-endorser-alpha-binary ${tezosBin}/tezos-endorser-${newSuffix} \
-      --second-accuser-alpha-binary  ${tezosBin}/tezos-accuser-${newSuffix} \
-      --tezos-client-binary ${tezosClient} \
-      --tezos-admin-client-binary ${tezosBin}/tezos-admin-client
-  '';
-
 in (obApp distroMethods.source) // {
-  inherit pkgs dockerExe kilnVMConfig dockerImage installKiln votingTest protocolTest;
+  tests = import ./tests { inherit obelisk pkgs; };
+
+  inherit pkgs dockerExe kilnVMConfig dockerImage installKiln;
   server = args@{ hostName, adminEmail, routeHost, enableHttps, version, ... }:
     let
       network =
---
+++ tests/default.nix
@@ -0,0 +1,65 @@
+{ obelisk ? (import ../tezos-bake-central/.obelisk/impl {})
+, pkgs ? obelisk.reflex-platform.nixpkgs
+}: let
+  tbp-flextesa = import dep/tbp-flextesa {};
+in {
+  voting = pkgs.writeScriptBin "voting-test" ''
+    #!/usr/bin/env bash
+    set -e
+    echo 'Starting flextesa voting test... monitor a node via kiln at http://127.0.0.1:20000'
+    rm -rf /tmp/kiln_voting_test
+    cp -r ${tbp-flextesa.tezos.master.tezos-src}/src/bin_client/test/proto_test_injection /tmp/kiln_voting_test
+    chmod -R +w /tmp/kiln_voting_test
+    ${tbp-flextesa.tezos.master.kit}/bin/tezos-sandbox voting \
+      /tmp/kiln_voting_test \
+      --base-port=20000 --interactive=true --pause-on-error=true
+  '';
+
+  protocol = let
+    tezosBin = tbp-flextesa.tezos.master.kit + /bin;
+    tezosClient = "${tezosBin}/tezos-client";
+    oldSuffix = "003-PsddFKi3";
+    newSuffix = "004-Pt24m4xi";
+    propto = (import dep/tbp-multi-protocol-mainnet {}).tezos.mainnet.tezos-src + /src/proto_004_Pt24m4xi/lib_protocol;
+  in pkgs.writeScriptBin "protocol-test" ''
+    #!/usr/bin/env bash
+    set -Eeuo pipefail
+
+    fail() { "''${___fail:?$1}"; }
+    contains_re_group() { [[ $1 =~ $2 ]] && echo "''${BASH_REMATCH[1]}"; }
+
+    connected_ledgers=''$(${tezosClient} -P 0 list connected ledgers 2>/dev/null)
+    ledger_uri=$(contains_re_group "$connected_ledgers" '(ledger://[^\"]+)' || fail "Unable to find a connected ledger")
+    echo "> Ledger: $ledger_uri"
+
+    show_ledger=$(${tezosClient} -P 0 show ledger "$ledger_uri" 2>/dev/null)
+    pk=$(contains_re_group "$show_ledger" '\* Public Key: ([A-Za-z0-9]+)' || fail "Unable to determine public key for $ledger_uri")
+    echo "> PK: $pk"
+    pkh=$(contains_re_group "$show_ledger" '\* Public Key Hash: ([A-Za-z0-9]+)' || fail "Unable to determine public key hash for $ledger_uri")
+    echo "> PKH: $pkh"
+
+    echo 'Starting flextesa protocol test...'
+    echo '>>> Monitor a node via Kiln at http://127.0.0.1:20000 <<<'
+
+    ${tezosBin}/tezos-sandbox daemons-upgrade ${propto} \
+      --add-bootstrap "LBK,$pk,$pkh,$ledger_uri@200_000_000_000" \
+      --no-daemons-for LBK \
+      --add-external 10000 \
+      --generate-kiln ./tezos-bake-central/config/,10000 \
+      --clean-kiln-config \
+      --time 4,4 \
+      --blocks-per-vot 40 \
+      --pause-on-error true \
+      --root-path "$PWD/dpu_root_path" \
+      --tezos-node-binary ${tezosBin}/tezos-node \
+      --protocol-hash PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP \
+      --first-baker-alpha-binary     ${tezosBin}/tezos-baker-${oldSuffix} \
+      --first-endorser-alpha-binary  ${tezosBin}/tezos-endorser-${oldSuffix}} \
+      --first-accuser-alpha-binary   ${tezosBin}/tezos-accuser-${oldSuffix}} \
+      --second-baker-alpha-binary    ${tezosBin}/tezos-baker-${newSuffix} \
+      --second-endorser-alpha-binary ${tezosBin}/tezos-endorser-${newSuffix} \
+      --second-accuser-alpha-binary  ${tezosBin}/tezos-accuser-${newSuffix} \
+      --tezos-client-binary ${tezosClient} \
+      --tezos-admin-client-binary ${tezosBin}/tezos-admin-client
+  '';
+}
