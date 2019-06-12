de5d5f5c048544589b77514cb225f0855fd7116a
Divam <dfordivam@gmail.com>
2019-06-12 10:54:51 +0900

    If process doesnt stop with SIGINT for 1 min, then send SIGKILL.

--- tezos-bake-central/backend/src/Backend/Workers/Process.hs
+++ tezos-bake-central/backend/src/Backend/Workers/Process.hs
@@ -34,6 +34,7 @@ import Rhyolite.Backend.DB (runDb)
 import Rhyolite.Backend.DB.PsqlSimple (queryQ, fromOnly)
 import Rhyolite.Backend.Logging (LoggingEnv (..), runLoggingEnv)
 import Rhyolite.Backend.Schema (fromId)
+import System.Posix.Signals (signalProcess, sigKILL)
 import System.Process (CreateProcess, withCreateProcess, getProcessExitCode, terminateProcess)
 import qualified System.Process as Proc
 import System.IO (hFlush, hGetLine)
@@ -147,7 +148,7 @@ processWorker initialize (Arg logger) (Arg db) (Arg appConfig) (Arg namespace) (
       withHandleCopyWith (logInfoNS namespace) hStdout $ do
         -- logInfoNS for stderr is intentional, the node prints the usual messages also on stderr
         withHandleCopyWith (logInfoNS namespace) hStderr $ do
-          runLoggingEnv logger go
+          runLoggingEnv logger $ go Nothing
       where
         withHandleCopyWith perLine h' f = case h' of
           Nothing -> f
@@ -160,8 +161,8 @@ processWorker initialize (Arg logger) (Arg db) (Arg appConfig) (Arg namespace) (
                     Right ln -> perLine (T.pack ln) *> loop
 
         {-# INLINE go #-}
-        go :: forall m1. (MonadLogger m1, MonadIO m1, MonadBaseNoPureAborts IO m1) => m1 ()
-        go = do
+        go :: forall m1. (MonadLogger m1, MonadIO m1, MonadBaseNoPureAborts IO m1) => Maybe Int -> m1 ()
+        go mCount = do
           let getPC = \case
                 [] -> ProcessControl_Stop
                 (c:_) -> c
@@ -170,10 +171,11 @@ processWorker initialize (Arg logger) (Arg db) (Arg appConfig) (Arg namespace) (
           liftIO (getProcessExitCode ph) >>= \case
             Nothing -> do
               updateState ProcessState_Running
-              case procControl of
-                ProcessControl_Run -> return ()
-                _ -> liftIO $ terminateProcess ph
-              threadDelay' 1 *> go
+              let stop = procControl /= ProcessControl_Run
+              liftIO $ when stop $ if mCount < Just 60
+                then terminateProcess ph
+                else Proc.getPid ph >>= mapM_ (signalProcess sigKILL)
+              threadDelay' 1 *> go (if stop then Just (maybe 1 (+ 1) mCount) else Nothing)
             Just _ -> case procControl of
               ProcessControl_Stop -> do
                 updateState ProcessState_Stopped
