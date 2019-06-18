36def9da67b4094c1261c5aa545ce9e6874bcd92
James Deikun <james.deikun@obsidian.systems>
2019-06-18 14:09:28 -0400

    Tez is a string in JSON

--- tezos-bake-monitor-lib/src/Tezos/Tez.hs
+++ tezos-bake-monitor-lib/src/Tezos/Tez.hs
@@ -34,12 +34,10 @@ microTez
 
 -- | the instance for Data.Fixed.Micro defined in Data.Aeson is perfectly
 -- cromulent, its just not what we need.  tezos encodes these values as
--- integers.  Like the FromJSON instance below, it "may" be neccesary to encode
--- values larger than `2^31/resolution` as strings, but that's not handled
--- currently
+-- strings.
 instance ToJSON Tez where
-  toJSON = toJSON . getMicroTez
-  toEncoding = toEncoding . getMicroTez
+  toJSON = toJSON . show . getMicroTez
+  toEncoding = toEncoding . show . getMicroTez
 
 instance FromJSON Tez where
   parseJSON x = microTez <$> parseIntegralAsString @Word64 x
