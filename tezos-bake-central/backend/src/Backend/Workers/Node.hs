7b1e8f4e3cf422d95150744f91a4f01a270d1e66
Divam <dfordivam@gmail.com>
2019-06-28 16:52:07 +0900

    Enable monitoring OS node if the option in nds is set

--- tezos-bake-central/backend/src/Backend/Workers/Node.hs
+++ tezos-bake-central/backend/src/Backend/Workers/Node.hs
@@ -359,7 +359,9 @@ updateDataSource
   :: forall m. (MonadIO m, MonadBaseNoPureAborts IO m)
   => NodeDataSource -> DataSource -> m ()
 updateDataSource nds (pn, chain, uri) = do
-  enabled <- publicNodeEnabled
+  enabled <- if pn == PublicNode_Obsidian && (_nodeDataSource_osPublicNode nds /= Nothing)
+    then pure True
+    else publicNodeEnabled
   -- TODO: prefer to get this from the database, or from private nodes before
   when enabled $ do
     _ <- liftIO $ initParams nds (Identity (Just pn, uri))
