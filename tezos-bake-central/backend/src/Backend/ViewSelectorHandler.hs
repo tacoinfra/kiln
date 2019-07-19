17bc71afc3bd950ac041dbfebbc6058481a27185
Divam <dfordivam@gmail.com>
2019-07-19 20:16:28 +0900

    Get alert count for each error log as Map

--- tezos-bake-central/backend/src/Backend/ViewSelectorHandler.hs
+++ tezos-bake-central/backend/src/Backend/ViewSelectorHandler.hs
@@ -27,6 +27,7 @@ import Data.Functor.Identity (Identity (..))
 import Data.Functor.Apply (liftF2)
 import Data.Dependent.Sum (DSum (..))
 import Data.List (intersperse)
+import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Map.Monoidal (MonoidalMap(..))
 import qualified Data.Map.Monoidal as MMap
@@ -477,14 +478,46 @@ getErrorLogsImpl flt intervalMap = do
     leftBiasedUnions = MMap.unionsWith const
 
 getAlertCount
-  :: (Monad m, PostgresRaw m)
-  => m (Maybe Int)
-getAlertCount =
-  fmap Pg.fromOnly . listToMaybe <$> [queryQ|
-    SELECT
-      COUNT(*)
-    FROM "ErrorLog" el
-    WHERE el.stopped IS NULL|]
+  :: forall m.
+  ( MonadLogger m
+  , PersistBackend m
+  )
+  => m (Map (Some LogTag) Int)
+getAlertCount = Map.fromList . concat <$> traverse (\(This lTag) -> do
+  (x, _) <- runQuery lTag
+  $(logDebugSH) x
+  pure $ (map (\(t, v) -> (This t, v)) x)) universe
+  where
+    {-# INLINE queryAlert #-}
+    queryAlert
+      :: forall f c b. (Monad f, PersistBackend f, MonadLogger f, PersistEntity b, EntityConstr b c)
+      => c (ConstructorMarker b)
+      -> [Some (Related b c)]
+      -> f ([Int], b)
+    queryAlert ctor _related = do
+      let
+        build :: [PersistValue] -> f Int
+        build = evalStateT $ do
+          StateT fromPersistValues
+        entityD = entityDef pg (undefined :: b)
+        constrD = constructors entityD !! constrNum
+        constrNum = entityConstrNum (Proxy @b) ctor
+        sqlTable = tableName id entityD constrD
+        qBase :: Utf8
+        qBase =
+          "SELECT \
+          \     COUNT(*) \
+          \ FROM \"ErrorLog\" el \
+          \ JOIN \"" <> sqlTable <> "\" t ON t.log = el.id \
+          \ WHERE el.stopped IS NULL"
+      $(logDebugSH) ("queryAlert" :: Text, sqlTable)
+      v <- traceQuery qBase id build
+      pure (v, undefined :: b)
+
+    runQuery :: LogTag e -> m ([(LogTag e, Int)] , DSum LogTag Identity)
+    runQuery lTag = do
+      vus <- logAssume lTag $ queryAlert (singleConstructor $ proxify lTag) (logDep lTag)
+      pure $ (\(vs, u) -> (map (\v -> (lTag, v)) vs, lTag :=> Identity u)) vus
 
 getBakerAddresses
   :: forall m. (PostgresRaw m, MonadIO m, PersistBackend m, MonadLogger m)
