d9ff1bb84a3603788c8c7af88a8ee724556ac53e
Divam <dfordivam@gmail.com>
2019-08-14 15:21:49 +0900

    Remove startBaking request, as this is done in backend automatically

--- tezos-bake-central/backend/src/Backend/RequestHandler.hs
+++ tezos-bake-central/backend/src/Backend/RequestHandler.hs
@@ -50,7 +50,7 @@ import Backend.Schema
 import qualified Backend.Telegram as Telegram
 import Backend.Upgrade (updateUpstreamVersion)
 import Backend.Workers.Node (DataSource, updateDataSource)
-import Backend.Workers.TezosClient (addBakerImpl, startBaking)
+import Backend.Workers.TezosClient (addBakerImpl)
 import Common.Api (PrivateRequest (..), PublicRequest (..))
 import Common.App
 import Common.Schema
@@ -100,7 +100,6 @@ requestHandler appConfig upgradeBranch emailFromAddr nds publicNodeSources =
         update [LedgerAccount_shouldSetupToBakeField =. True] (embeddedSecretKeyEquals LedgerAccount_secretKeyField sk)
       PublicRequest_RegisterKeyAsDelegate sk fee -> inDb $ do
         update [LedgerAccount_shouldRegisterFeeField =. Just fee] (embeddedSecretKeyEquals LedgerAccount_secretKeyField sk)
-      PublicRequest_StartBaking pkh -> inDb $ startBaking pkh
       PublicRequest_SetHWM sk bl -> inDb $ do
         update [LedgerAccount_shouldSetHWMField =. Just bl] (embeddedSecretKeyEquals LedgerAccount_secretKeyField sk)
 
