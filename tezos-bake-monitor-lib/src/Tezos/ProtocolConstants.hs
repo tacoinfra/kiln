d95284967ec38a86d1e3a568090948f31fdc5638
Divam <dfordivam@gmail.com>
2019-08-23 12:47:00 +0900

    Remove TODO comments, and a comment

--- tezos-bake-monitor-lib/src/Tezos/ProtocolConstants.hs
+++ tezos-bake-monitor-lib/src/Tezos/ProtocolConstants.hs
@@ -82,7 +82,6 @@ instance ToJSON ProtoInfo where
   toJSON = $(Aeson.mkToJSON tezosJsonOptions ''ProtoInfo)
   toEncoding = $(Aeson.mkToEncoding tezosJsonOptions ''ProtoInfo)
 
--- TODO: DELETE
 -- | Convert a level to the cycle that contains it.
 --
 -- We subtract 1 because the first cycle begins after the genesis block. Yet
@@ -92,7 +91,6 @@ levelToCycle :: ProtoInfo -> RawLevel -> Cycle
 levelToCycle params (RawLevel l) = Cycle $ max 0 (l - 1) `div` unRawLevel (params ^. protoInfo_blocksPerCycle)
 
 
--- TODO: DELETE
 -- | Convert a cycle to the level of the first block in that cycle.
 --
 -- We add 1 because we do not consider the genesis block as part of the first
@@ -101,7 +99,6 @@ levelToCycle params (RawLevel l) = Cycle $ max 0 (l - 1) `div` unRawLevel (param
 firstLevelInCycle :: ProtoInfo -> Cycle -> RawLevel
 firstLevelInCycle params cycl = 1 + fromIntegral cycl * params ^. protoInfo_blocksPerCycle
 
--- TODO: DELETE
 -- | We want the first block in the cycle that sits PRESERVED_CYCLES before the
 -- requested level, that is on the correct branch.
 rightsContextLevel :: ProtoInfo -> RawLevel -> RawLevel
