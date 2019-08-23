283d8d6beb4fb9294dd9857a734b81dfeae5dfa6
Jonathan D.K. Gibbons <jonored@gmail.com>
2019-08-22 23:28:55 -0400

    Add helper shim for calling multiple endpoints.

--- tezos-bake-monitor-lib/src/Tezos/Michelson.hs
+++ tezos-bake-monitor-lib/src/Tezos/Michelson.hs
@@ -1,20 +1,26 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ViewPatterns #-}
 module Tezos.Michelson where
 
+import Control.Applicative
+import Control.Monad (guard)
 import qualified Data.ByteString as BS
-import Data.Sequence (Seq(..))
+import Data.Maybe
+import Data.Sequence (Seq(..), elemIndexL, ViewL(..), viewl)
 import qualified Data.Text as T
 import Prelude hiding (pattern Left, pattern Right)
 
 import Tezos.Base16ByteString (Base16ByteString(..))
 import qualified Tezos.Binary as B
-import Tezos.Micheline (Expression(..), MichelinePrimAp(..), MichelinePrimitive(..))
+import Tezos.Micheline (Expression(..), MichelinePrimAp(..), MichelinePrimitive(..), Annotation(..))
+import Tezos.Contract (ContractScript(..))
 
 pattern Prim :: T.Text -> Seq Expression -> Expression
-pattern Prim p a = Expression_Prim (MichelinePrimAp (MichelinePrimitive p) a Empty)
+pattern Prim p a <- Expression_Prim (MichelinePrimAp (MichelinePrimitive p) a _)
+  where Prim p a = Expression_Prim (MichelinePrimAp (MichelinePrimitive p) a Empty)
 
 pattern Prim0 :: T.Text -> Expression
 pattern Prim0 p = Prim p Empty
@@ -43,3 +49,22 @@ pattern Bytes x = Expression_Bytes (Base16ByteString x)
 
 pattern AsBytes :: B.TezosBinary a => a -> Expression
 pattern AsBytes x = Bytes (B.TezosBinary x)
+
+findInOrTree :: T.Text -> Expression -> Maybe (Expression -> Expression)
+findInOrTree annot = \case
+  Prim2 "or" r l -> ((Left .) <$> findInOrTree annot l) <|> ((Right .) <$> findInOrTree annot r)
+  Expression_Prim (MichelinePrimAp (MichelinePrimitive _) _ annots) -> do
+    guard $ isJust $ Annotation_Field annot `elemIndexL` annots
+    pure id
+  _ -> fail "Could not find expression"
+
+wrapEndpointCall :: T.Text -> ContractScript -> Expression -> Maybe Expression
+wrapEndpointCall endpoint ContractScript { _contractScript_code = code } =
+  (endpointWrapper <*>) . pure
+  where 
+    endpointWrapper = do
+      -- This is assuming that the contract goes [parameter, storage, code] but that seems to currently be valid.
+      Expression_Seq codeParts <- pure code
+      Prim1 "parameter" param :< _ <- pure $ viewl codeParts
+      findInOrTree endpoint param
+
