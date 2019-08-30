1bb18f0e4eb5259b57dd87d106935b60bc140ed0
Jonathan D.K. Gibbons <jonored@gmail.com>
2019-08-30 13:17:01 -0400

    Cleanup of callView handler.
    It still has an odd return value, but that can wait.

--- tezos-noderpc/src/Tezos/NodeRPC/Helpers.hs
+++ tezos-noderpc/src/Tezos/NodeRPC/Helpers.hs
@@ -43,7 +43,21 @@ import Tezos.Micheline
 import Tezos.Michelson
 import Tezos.Account
 
-dryRunEndpoint :: (MonadIO m, MonadError e m, AsRpcError e, MonadReader s m, HasNodeRPC s, MonadLogger m, ToMicheline a) => ChainId -> BlockHash -> ContractId -> ContractId -> Text -> a -> m OperationWithMetadata
+dryRunEndpoint
+  :: (MonadIO m
+    , MonadError e m
+    , AsRpcError e
+    , MonadReader s m
+    , HasNodeRPC s
+    , MonadLogger m
+    , ToMicheline a)
+  => ChainId
+  -> BlockHash
+  -> ContractId
+  -> ContractId
+  -> Text
+  -> a
+  -> m OperationWithMetadata
 dryRunEndpoint chain block account contract endpoint argument = do
   protocolConstants <- nodeRPC $ rProtoConstants chain block
   let gas_max = _protoInfo_hardGasLimitPerOperation protocolConstants
@@ -51,20 +65,50 @@ dryRunEndpoint chain block account contract endpoint argument = do
   callingAccount <- nodeRPC $ rContract account ChainTag_Main block
   let counter = succ $ _account_counter callingAccount
   contractAccount <- nodeRPC $ rContract contract ChainTag_Main block
-  Just contractParameter <- pure $ flip (wrapEndpointCall endpoint) (toMicheline argument) =<< (_account_script contractAccount)
-  let opTransfer = OpContentsTransaction 0 contract $ Just contractParameter
-  let opContents = OpContentsList_Single $ OpContents_Transaction $ OpContentsManager account 10 counter gas_max storage_max opTransfer
+  case flip (wrapEndpointCall endpoint) (toMicheline argument) =<< _account_script contractAccount of
+    Nothing -> error "Could not find endpoint"
+    Just contractParameter -> do
+      let opTransfer = OpContentsTransaction 0 contract $ Just contractParameter
+          opContents = OpContentsList_Single $ OpContents_Transaction $ OpContentsManager account 10 counter gas_max storage_max opTransfer
       -- This needs to fit the format of a valid signature, but is never looked at past that.
-      dummySignature = Just "edsigtXomBKi5CTRf5cjATJWSyaRvhfYNHqSUGrn4SdbYRcGwQrUGjzEfQDTuqHhuA8b2d8NarZjz8TRf65WkpQmo423BtomS8Q"
-  let op = (OpsKindTag_Single (OpKindTag_Manager OpKindManagerTag_Transaction)) :=> Op { _op_branch = block, _op_contents = opContents, _op_signature = dummySignature }
-  nodeRPC $ rRunOperation chain block op
+          dummySignature = Just "edsigtXomBKi5CTRf5cjATJWSyaRvhfYNHqSUGrn4SdbYRcGwQrUGjzEfQDTuqHhuA8b2d8NarZjz8TRf65WkpQmo423BtomS8Q"
+          op = (OpsKindTag_Single (OpKindTag_Manager OpKindManagerTag_Transaction)) :=> Op { _op_branch = block, _op_contents = opContents, _op_signature = dummySignature }
+      nodeRPC $ rRunOperation chain block op
 
 
-callViewEndpoint :: (MonadIO m, MonadError e m, AsRpcError e, MonadReader s m, HasNodeRPC s, MonadLogger m, ToMicheline a, FromMicheline b) => ChainId -> BlockHash -> ContractId -> ContractId -> ContractId -> Text -> a -> m (Either String b)
+callViewEndpoint
+  :: ( MonadIO m
+     , MonadError e m
+     , AsRpcError e
+     , MonadReader s m
+     , HasNodeRPC s
+     , MonadLogger m
+     , ToMicheline a
+     , FromMicheline b)
+  => ChainId
+  -> BlockHash
+  -> ContractId
+  -> ContractId
+  -> ContractId
+  -> Text
+  -> a
+  -> m (Either String b)
 callViewEndpoint chain block account contract tgtContract endpoint argument = do
   let finalArgument = Pair (toMicheline argument) $ toMicheline $ tgtContract
   result <- dryRunEndpoint chain block account contract endpoint finalArgument
-  let Just internalOperationResults = join $ fmap (firstOf traverse) $ join $ result ^? operationWithMetadata_contents . folded . _OperationContents_Transaction . operationContentsTransaction_metadata . managerOperationMetadata_internalOperationResults
-      (Just resultExpr) = join $ internalOperationResults ^? _InternalOperationResult_Transaction . internalOperationContentsTransaction_result . operationResultTransaction_storage
-  return $ fromMicheline resultExpr
-
+  case result ^? valueFromResult of
+    Just expr -> return $ fromMicheline expr
+    _ -> error "Couldn't read operation"
+  where
+    valueFromResult
+      = operationWithMetadata_contents
+      . traverse
+      . _OperationContents_Transaction
+      . operationContentsTransaction_metadata
+      . managerOperationMetadata_internalOperationResults
+      . _Just
+      . traverse
+      . _InternalOperationResult_Transaction
+      . internalOperationContentsTransaction_result
+      . operationResultTransaction_storage
+      . _Just
