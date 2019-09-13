bfaa0701de235972a5c566e39942efa167dafa80
Jonathan D.K. Gibbons <jonored@gmail.com>
2019-09-12 21:44:44 -0400

    Core signing function for tz1 accounts.

---
+++ tezos-noderpc/src/Tezos/Signature/Sign.hs
@@ -0,0 +1,38 @@
+module Tezos.Signature.Sign where
+
+import Control.Applicative
+import Data.Aeson
+import qualified Data.ByteArray as M
+import qualified Data.ByteString as BS
+import Data.Maybe (fromMaybe)
+
+import Crypto.Error (onCryptoFailure)
+import qualified Crypto.MicroECC as U
+import qualified Crypto.PubKey.Ed25519 as Ed25519
+import qualified Crypto.Hash as H
+
+import Tezos.Base58Check
+import Tezos.PublicKey
+import Tezos.ShortByteString (ShortByteString, fromShort, toShort)
+import Tezos.Signature
+
+data SecretKeyInMemory
+  = SecretKey_Ed25519Seed Ed25519Seed
+  | SecretKey_Ed25519 Ed25519SecretKey
+  deriving (Show, Eq)
+
+instance FromJSON SecretKeyInMemory where
+  parseJSON v = (SecretKey_Ed25519Seed <$> parseJSON v) <|> (SecretKey_Ed25519 <$> parseJSON v)
+
+sign :: SecretKeyInMemory -> BS.ByteString -> Maybe Signature
+sign key msg = case key of
+  SecretKey_Ed25519Seed sk -> sign_Ed25519 (fromShort $ unHashedValue sk) digest
+-- We could be faster and more error-checking here by not recalculating the public key, but this works.
+  SecretKey_Ed25519 sk -> sign_Ed25519 (BS.take 32 . fromShort $ unHashedValue sk) digest
+  where
+    digest = M.convert $ H.hashWith H.Blake2b_256 msg
+
+sign_Ed25519 :: BS.ByteString -> BS.ByteString -> Maybe Signature
+sign_Ed25519 sk msg = onCryptoFailure (const Nothing) Just $ do
+    edSk <- Ed25519.secretKey sk
+    pure $ Signature_Ed25519 . HashedValue . toShort . M.convert $ Ed25519.sign edSk (Ed25519.toPublic edSk) msg
