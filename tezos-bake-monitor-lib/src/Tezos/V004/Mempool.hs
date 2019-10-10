32faa290377b0cc0c4914ae6994687cf02932fce
Hector Escobedo <hector.escobedo@obsidian.systems>
2019-10-10 11:42:45 -0400

    Use direct JSON conversion for Mempool.

--- tezos-bake-monitor-lib/src/Tezos/V004/Mempool.hs
+++ tezos-bake-monitor-lib/src/Tezos/V004/Mempool.hs
@@ -5,7 +5,6 @@
 
 module Tezos.V004.Mempool where
 
-import Data.Aeson
 import Data.Aeson.TH
 import Data.Dependent.Sum
 import GHC.Generics
@@ -16,20 +15,11 @@ import Tezos.Common.Json
 import Tezos.V004.Operation
 
 data Mempool = Mempool
-  { _mempool_applied :: [Result (DSum OpsKindTag PendingOp)]
-  , _mempool_refused :: [Result (OperationHash, DSum OpsKindTag ErroredOp)]
-  , _mempool_branchRefused :: [Result (OperationHash, DSum OpsKindTag ErroredOp)]
-  , _mempool_branchDelayed :: [Result (OperationHash, DSum OpsKindTag ErroredOp)]
-  , _mempool_unprocessed :: [Result (OperationHash, DSum OpsKindTag ProtoOp)]
+  { _mempool_applied :: [DSum OpsKindTag PendingOp]
+  , _mempool_refused :: [(OperationHash, DSum OpsKindTag ErroredOp)]
+  , _mempool_branchRefused :: [(OperationHash, DSum OpsKindTag ErroredOp)]
+  , _mempool_branchDelayed :: [(OperationHash, DSum OpsKindTag ErroredOp)]
+  , _mempool_unprocessed :: [(OperationHash, DSum OpsKindTag ProtoOp)]
   } deriving Generic
-
-instance FromJSON (Result (DSum OpsKindTag PendingOp)) where
-  parseJSON = pure . fromJSON
-
-instance FromJSON (Result (OperationHash, DSum OpsKindTag ErroredOp)) where
-  parseJSON = pure . fromJSON
-
-instance FromJSON (Result (OperationHash, DSum OpsKindTag ProtoOp)) where
-  parseJSON = pure . fromJSON
 
 deriveFromJSON tezosJsonOptions 'Mempool
