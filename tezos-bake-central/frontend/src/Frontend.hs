c7e3c2a81428ec94eccf8df031c91da1394e4e3f
Ben Kolera <ben.kolera@gmail.com>
2019-10-27 15:48:20 +1000

    fix: Only runs ledger check if we have a baker
    Also fixes a few bugs:
    - Makes the getBakerNextRights not crash when there are no rights
    - Adds LedgerDisconnected Errors to the BakerError universe
    - Moves the ledger device banner down 0.2em so it lines up to the others

--- tezos-bake-central/frontend/src/Frontend.hs
+++ tezos-bake-central/frontend/src/Frontend.hs
@@ -414,15 +414,20 @@ appHeader = SemUi.segment (def & SemUi.segmentConfig_vertical SemUi.|~ True) $ d
             text "/"
             display $ unCycle . cyclesPerPeriod <$> protoInfo
             dyn_ $ ffor (isVotingPeriod <$> kind) $ flip when $ elClass "i" "blue icon-vote-badge icon" blank
-      elAttr "div" ("class" =: "item" <> "style" =: "position: relative") $ divClass "content" $ do
-        dCl <- watchConnectedLedger
-        let dIsLedgerConnected = isJust . (_connectedLedger_ledgerIdentifier =<<) <$> dCl
-        divClass "header" $ do
-          iconDyn $ ffor dIsLedgerConnected $ bool "red x" "green check"
-          elAttr "img" ("src" =: static @"images/ledger.svg" <> "class" =: "ledger") blank
-        divClass "description" $ do
-          text "Ledger Device "
-          dynText $ ffor dIsLedgerConnected $ bool "Disconnected" "Connected"
+
+      internalBakerMayDyn <- watchInternalBaker
+
+      dyn_ $ ffor internalBakerMayDyn $ \internalBakerMay ->
+        when (isJust internalBakerMay) $ do
+          elAttr "div" ("class" =: "item" <> "style" =: "position: relative") $ divClass "content" $ do
+            dCl <- watchConnectedLedger
+            let dIsLedgerConnected = isJust . (_connectedLedger_ledgerIdentifier =<<) <$> dCl
+            divClass "header" $ do
+              iconDyn $ ffor dIsLedgerConnected $ bool "red x" "green check"
+              elAttr "img" ("src" =: static @"images/ledger.svg" <> "class" =: "ledger") blank
+            divClass "description" $ do
+              text "Ledger Device "
+              dynText $ ffor dIsLedgerConnected $ bool "Disconnected" "Connected"
 
   headerBell
 
