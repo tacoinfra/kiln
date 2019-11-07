aa91fceb57957817c4a7fdb925853196562e59e5
Divam <dfordivam@gmail.com>
2019-11-07 16:51:48 +0900

    Fix voting-test for amendement popup

--- tests/default.nix
+++ tests/default.nix
@@ -17,6 +17,7 @@ in {
       /tmp/kiln_voting_test/TEZOS_PROTOCOL /tmp/kiln_voting_test/TEZOS_PROTOCOL \
       --base-port=20000 \
       --interactive=true \
+      --winning-client-is-clueless \
       --pause-on-error=true
   '';
 
