5971c6b7b88c3f280f3daa70bde7abbc89ca2c63
Ben Kolera <ben.kolera@gmail.com>
2019-11-09 05:28:01 +1000

    Lets try to put the system into the old nixpkgs
    Adding in a missing semicolon. Sadness.

--- tezos-bake-central/static/semantic-ui.nix
+++ tezos-bake-central/static/semantic-ui.nix
@@ -1,13 +1,15 @@
-{pkgs ? (import ../.obelisk/impl {}).reflex-platform.nixpkgs} :
+{ pkgs ? (import ../.obelisk/impl {}).reflex-platform.nixpkgs
+, system ? builtins.currentSystem
+} :
 let
   semantic-ui = ./semantic-ui;
-  old-pkgs = import ../dep/old-nixpkgs {};
+  old-pkgs = import ../dep/old-nixpkgs { inherit system; };
   semantic-ui-env = import ./semantic-ui-env {pkgs = old-pkgs;};
 in {
   bakemonitor-semantic-ui = pkgs.runCommand "bakemonitor-semantic-ui" {} ''
     ln -s ${semantic-ui-env.package}/lib/node_modules/semantic-ui/node_modules node_modules
     cp -r ${semantic-ui}/{gulpfile.js,semantic.json,src,tasks} ./
-    ${old-pkgs.nodejs-6_x}/bin/node node_modules/.bin/gulp build
+    node_modules/.bin/gulp build
 
     cp -r dist $out
 
