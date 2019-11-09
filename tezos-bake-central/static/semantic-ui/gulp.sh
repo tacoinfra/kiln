abf7452e01ebd8f9046148d7f747425872e72a74
Ben Kolera <ben.kolera@gmail.com>
2019-11-09 10:29:11 +1000

    fix: Removes garbage debug statements from gulp.sh

--- tezos-bake-central/static/semantic-ui/gulp.sh
+++ tezos-bake-central/static/semantic-ui/gulp.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 rm node_modules
 ln -sf $(nix-build ../semantic-ui.nix -A package --no-out-link)/lib/node_modules/semantic-ui/node_modules node_modules
-printenv
-head ./node_modules/.bin/gulp
 ./node_modules/.bin/gulp "$@"
