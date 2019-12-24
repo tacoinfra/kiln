0185ae14393d928ad8cae0aeb34185d8c231dbc3
Divam <dfordivam@gmail.com>
2019-12-24 15:44:44 +0900

    Bump to v0.7.4

--- default.nix
+++ default.nix
@@ -207,6 +207,6 @@ in (obApp distroMethods.source) // {
     obApp = obAppGargoyle distroMethods.linuxPackage;
     nodeKit = tezosScopedKit;
     pkgName = "kiln";
-    version = "0.7.3"; # TODO: Calculate this
+    version = "0.7.4"; # TODO: Calculate this
   }).kiln-debian;
 }
