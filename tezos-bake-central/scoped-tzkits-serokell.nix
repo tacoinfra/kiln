fb236b5bde7c985104a6e3312671cdf6bfa9cdf3
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2020-07-13 13:13:47 -0400

    Binaries are appropriately made executable in nix store

--- tezos-bake-central/scoped-tzkits-serokell.nix
+++ tezos-bake-central/scoped-tzkits-serokell.nix
@@ -1,11 +1,10 @@
 {pkgs, tezos-binaries }:
-
 let
   binaries = tezos-binaries;
   in pkgs.runCommand "tezosScopedKit_" {} ''
   mkdir -p $out/bin
   for bin in $(ls ${binaries}) ; do
-    chmod 755 ${binaries}/$bin
-    ln -s ${binaries}/$bin $out/bin/multinetwork-$bin
+    cp ${binaries}/$bin $out/bin/multinetwork-$bin
+    chmod +x $out/bin/multinetwork-$bin
   done
   ''
