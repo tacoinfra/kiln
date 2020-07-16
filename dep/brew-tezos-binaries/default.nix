c6e352b711c887ba8f0e8ef6c33fadd1aeeaad88
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2020-07-16 12:39:52 -0400

    Can get brew binaries (derivation gets built!)

--- dep/brew-tezos-binaries/default.nix
+++ dep/brew-tezos-binaries/default.nix
@@ -1,37 +1,19 @@
-########################################
-# THIS IS NOT EVEN CLOSE TO BEING DONE #
-########################################
 { stdenv }:
-    # this is just a temporary hack
-    # PLEASE MAKE SURE THAT THIS FILE EXISTS (brewDirectory)
-    let brewDirectory = "/usr/local/Cellar/tezos/7.2/bin/";
-    in if builtins.pathExists brewDirectory
-       then stdenv.mkDerivation rec {
-              name = "tezos-binaries-${version}";
-              version = "7.2-1";
 
-              src = builtins.fetchurl {url = "file://" + brewDirectory; sha256 = "0mdqa9w1p6cmli6976v4wi0sw9r4p5prkj7lzfd1877wk11c9c73";};
+stdenv.mkDerivation rec {
+  name = "tezos-binaries-${version}";
+  version = "7.2-1";
 
-              installPhase = ''
-              mkdir -p $out/bin
-              for bin in $(ls ${src}) ; do
-                cp ${src}/$bin $out/bin/multinetwork-$bin
-              done
-              chmod +x $out/bin
-              '';
+  src = builtins.fetchTarball {
+      url = "https://tqtezos.bintray.com/bottles-tq/tezos-7.2.catalina.bottle.3.tar.gz";
+      sha256 = "12b7c4hivwnn3lpc5cyznsnvglgjqc37dx2m9cmkzvg6yiippgph";
+      };
 
-                 }
-       else builtins.abort "brew-tezos-binaries.nix: Cannot find tezos-binaries"
-
-
-              # for bin in $(ls $out/bin) ; do
-                # mv $out/bin/$bin $out/bin/multinetwork-$bin
-              # done
-
-              # for bin in $(ls ${src}) ; do
-              #   cp ${src}/$bin $out/bin/multinetwork-$bin
-              #   chmod +x $out/bin/multinetwork-$bin
-              # done
-##############################################
-# THIS IS NOT EVEN BEING CLOSE TO BEING DONE #
-##############################################
+  installPhase = ''
+  mkdir -p $out/bin
+  for bin in $(ls ${src}/7.2/bin) ; do
+    cp ${src}/7.2/bin/$bin $out/bin/multinetwork-$bin
+    chmod +x $out/bin/multinetwork-$bin
+  done
+  '';
+  }
