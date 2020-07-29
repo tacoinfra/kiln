e54b99367529f343662ed67234fec1afdfde7da1
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2020-07-29 15:00:57 -0400

    serokell bump

--- dep/serokell-tezos-binaries/default.nix
+++ dep/serokell-tezos-binaries/default.nix
@@ -2,11 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "tezos-${version}";
-  version = "7.2-1";
+  version = "7.3-1";
 
   src = builtins.fetchTarball {
-      url = "https://github.com/serokell/tezos-packaging/releases/download/v${version}/binaries-${version}.tar.gz";
-      sha256 = "1xxyxnyc8wzliddfqwhj83k44pnhg74a03ii7a9r96rnfwyd9sam";
+      # Put this line back in once the official release is out! (The sha256 hash may change too)
+      # url = "https://github.com/serokell/tezos-packaging/releases/download/v${version}/binaries-${version}.tar.gz";
+      url = "https://github.com/serokell/tezos-packaging/releases/download/auto-release/binaries-${version}.tar.gz";
+      sha256 = "0q9yb1krq2a0fb7lpap4ga34dxr81hc6gn6vyddd52xplqyxjwdl";
       };
 
   installPhase = ''
