3e79a1ce9117827c12ee5b8eaed050100d861c55
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2020-07-30 15:40:39 -0400

    Hlint was complaining. I appeased it!

--- tezos-bake-central/frontend/src/Frontend.hs
+++ tezos-bake-central/frontend/src/Frontend.hs
@@ -291,7 +291,7 @@ appSideHeader =
     $ do
         SemUi.header def $ do
           kilnLogo
-          (blackhrefLink "https://tezos-kiln.org/" $ text appName)
+          blackhrefLink "https://tezos-kiln.org/" $ text appName
         SemUi.menu
           (def
             & SemUi.menuConfig_vertical SemUi.|~ True
