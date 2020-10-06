52baee731548ba803ffebbdb376a6ee5cecf0831
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2020-10-06 18:18:21 -0400

    Remove this files from the installed nix/store in the debian build

--- linux-distros.nix
+++ linux-distros.nix
@@ -78,6 +78,22 @@ let
           storePaths=$(${pkgs.perl}/bin/perl ${pkgs.pathsFromGraph} closure)
           mkdir -p $DEBDIR/${nix-store-root}/nix/store
           cp -prd $storePaths $DEBDIR/${nix-store-root}/nix/store/
+          # minize the closure size
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-ghc-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-gcc-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-python-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-perl-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-nodejs-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-webkitgtk-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-gst-plugins-base-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-gtk+3-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-cups-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-gdk-pixbuf-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-alsa-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-cairo-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-libvorbis-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-gstreamer-*
+          rm -rf $DEBDIR/${nix-store-root}/nix/store/*-cups-*
 
           chmod 0755 $DEBDIR/usr/bin/*
 
