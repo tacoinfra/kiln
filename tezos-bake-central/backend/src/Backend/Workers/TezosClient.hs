0aabe5f4a6b4638d1645477dc2170f4e3aa9f2fa
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2021-01-28 15:36:42 -0500

    use logDebugNS for logging failures/warnings in runClientCommand

--- tezos-bake-central/backend/src/Backend/Workers/TezosClient.hs
+++ tezos-bake-central/backend/src/Backend/Workers/TezosClient.hs
@@ -469,12 +469,12 @@ runClientCommand appConfig maybePaths mTimeout args handleError = do
       withTimeout run handle = flip (maybe ((liftIO run) >>= handle)) mTimeout $ \(t, err) -> (liftIO $ timeout' t run) >>= \case
         Just v -> handle v
         Nothing -> do
-          $(logWarn) $ "runClientCommand Timedout"
+          logDebugNS "kiln-node" "runClientCommand Timedout"
           throwError err
   withTimeout runProc $ \(exitCode, stdout, stderr) -> case exitCode of
     ExitSuccess -> pure $ T.strip stdout
     ExitFailure _ -> do
-      $(logWarn) $ "runClientCommand failed: " <> stderr
+      logDebugNS "kin-debugging" $ "runClientCommand failed: " <> stderr
       let strippedLines = fmap T.strip $ T.lines stderr
           warnings = takeWhile (/= "Error:") $ drop 1 $ dropWhile (/= "Warning:") strippedLines
           errors = filter (/= "Error:") $ dropWhile (/= "Error:") strippedLines
@@ -482,7 +482,7 @@ runClientCommand appConfig maybePaths mTimeout args handleError = do
       case handleError warnings (fatal ++ errors) of
         Right t -> pure t
         Left e -> do
-          $(logWarn) $ T.pack $ show e
+          logDebugNS "kiln-debugging" $ T.pack $ show e
           throwError e
 
 setupLedgerToBake :: (MonadLoggerIO m) => AppConfig -> Maybe BinaryPaths -> m SetupLedgerToBakeStep
