4824a484e6ec283e58388773eaa2bf299f06c31c
Igor Tkach <igor.tkach@tqtezos.com>
2021-01-29 15:20:26 -0500

    Update Maintainer field for Debian

--- linux-distros.nix
+++ linux-distros.nix
@@ -6,7 +6,7 @@
 , zcash
 }:
 let
-  maintainer = "Obsidian Systems <tezos@obsidian.systems>";
+  maintainer = "Tezos Kiln <maintainer@tezos-kiln.org>";
   description = "Application for participating in the Tezos blockchain.\n  Kiln is an application for participating in the Tezos blockchain. It allows the user to run a node, produce and validate blocks, and vote on protocol amendments through its interface. It can also be used to monitor existing Tezos infrastructure, and sends notifications via Telegram or SMTP when It detects an issue.";
 
   var-prefix = "/var/lib/${pkgName}";
