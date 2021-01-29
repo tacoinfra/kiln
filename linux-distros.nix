e00c6e8af9d775f71f8dd90a6cedb15d96d8325d
Igor Tkach <igor.tkach@tqtezos.com>
2021-01-29 15:22:04 -0500

    Update source to point to current repo

--- linux-distros.nix
+++ linux-distros.nix
@@ -185,7 +185,7 @@ let
   deb-copyright = pkgs.writeTextFile { name = "${pkgName}-deb-copyright"; text = ''
     Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
     Upstream-Name: Kiln
-    Source: https://gitlab.com/obsidian.systems/kiln
+    Source: https://gitlab.com/tezos-kiln/kiln
 
     Files: *
     Copyright: 2019 obsidian.systems
