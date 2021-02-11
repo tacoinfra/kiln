59f12e66fdf3dee05ff32c913afe7d46f05976c9
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2021-02-11 16:17:11 -0500

    new changes for edonet fork
    * update chain_id for edonet in tezos-bake-monitor-lib
    * update protocol hash and baker/endorser binary names
    * point to new binaries produced from serokell

--- dep/platform-specific-binaries.nix
+++ dep/platform-specific-binaries.nix
@@ -1,9 +1,7 @@
 { system ? builtins.currentSystem, pkgs, source }:
 
 let system-binaries = {
-                x86_64-linux =  if source
-                    then (import ./build-serokell-tezos-binaries).binaries
-                    else pkgs.callPackage ./serokell-tezos-binaries {};
+                x86_64-linux = pkgs.callPackage ./serokell-tezos-binaries {};
                 x86_64-darwin = pkgs.callPackage ./brew-tezos-binaries {};
                 };
 
--- dep/serokell-tezos-binaries/default.nix
+++ dep/serokell-tezos-binaries/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "tezos-${version}";
-  version = "8.1-1";
+  version = "8.2-1";
 
   src = builtins.fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/v${version}/binaries-${version}.tar.gz";
-      sha256 = "1gcxihqw61b541zmwpkqklbbj2zddkzcmg9i937kx14h914kc03m";
+      sha256 = "0yin1j97x04jzmf7ma8s77lcgyr2kfv1rxm7q6cm4364vfa1x55r";
       };
 
   installPhase = ''
