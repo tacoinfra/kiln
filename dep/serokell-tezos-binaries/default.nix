fc7f90a63cae84b82f61da6e6c63ce2b741587f6
Igor Tkach <igor.tkach@tqtezos.com>
2021-04-15 18:50:21 -0400

    update tezos binaries for linux

--- dep/serokell-tezos-binaries/default.nix
+++ dep/serokell-tezos-binaries/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "tezos-${version}";
-  version = "8.3";
+  version = "9.0-rc2";
 
   src = builtins.fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/v${version}-1/binaries-${version}-1.tar.gz";
-      sha256 = "1cfvw417b1cqniahscd7aqpb9qc8igkz5w0rkrmiznmwzlyamcj0";
+      sha256 = "1griwfrzb92hkpygg1anpqsi2gvw1vdlg4a48mqdjh6c54bj6kf3";
       };
 
   installPhase = ''
