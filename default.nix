093ad2b2ffc17e264fd309a0eb3b097b0e525b65
Igor Tkach <igor.tkach@tqtezos.com>
2021-04-15 18:49:52 -0400

    bump kiln version

--- default.nix
+++ default.nix
@@ -224,7 +224,7 @@ in (obApp false distroMethods.source system) // {
     obApp = obAppGargoyle false distroMethods.linuxPackage "x86_64-linux";
     nodeKit = tezosScopedKit false "x86_64-linux";
     pkgName = "kiln";
-    version = "0.8.6"; # TODO: Calculate this
+    version = "0.9.0"; # TODO: Calculate this
     inherit zcash;
   }).kiln-debian;
 
