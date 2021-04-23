fadad6645c0f7cc13358c6497fd9a5afe768a22e
Igor Tkach <igor.tkach@tqtezos.com>
2021-04-23 11:33:10 -0400

    clear out linux binary hash

--- dep/serokell-tezos-binaries/default.nix
+++ dep/serokell-tezos-binaries/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "tezos-${version}";
-  version = "9.0-rc2";
+  version = "9.0";
 
   src = builtins.fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/v${version}-1/binaries-${version}-1.tar.gz";
-      sha256 = "04x4crbc6y2gicmkll4vxx0qpy068xf0x8s64imdbc3xw898bb4k";
+      sha256 = "0000000000000000000000000000000000000000000000000000";
       };
 
   installPhase = ''
