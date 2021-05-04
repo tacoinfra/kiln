1c6d4c6ad10f771dcf8e8f1ca97a2ffc5ff14bef
Igor Tkach <igor.tkach@tqtezos.com>
2021-05-04 12:10:45 -0400

    update linux binaries tarball hash

--- dep/serokell-tezos-binaries/default.nix
+++ dep/serokell-tezos-binaries/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
 
   src = builtins.fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/v${version}-1/binaries-${version}-1.tar.gz";
-      sha256 = "0000000000000000000000000000000000000000000000000000";
+      sha256 = "13ckx0xvgvdlwl33ihdd12qydirjl4b5dd655i0l8vgp4fzm3ybi";
       };
 
   installPhase = ''
