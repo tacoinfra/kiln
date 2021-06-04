daa94b9781bb5f62a0520e4760c74ee92a9fdf34
Roman Melnikov <roman.melnikov@serokell.io>
2021-06-04 10:12:51 +0300

    Adapt new RPC changes
    Problem: New protocol introduces some changes in RPC interface.
    
    Solution: At first, these changes were supported in tezos-bake-monitor-lib.
    In kiln we bump used tezos-bake-monitor-lib revision and adapt the code
    to use the updated RPC interface.

--- tezos-bake-central/frontend/src/Frontend/Common.hs
+++ tezos-bake-central/frontend/src/Frontend/Common.hs
@@ -429,7 +429,7 @@ tzStatsBlockUri chain path = (`appendPaths` [path]) $ case chain of
   NamedChain_Edonet -> [uri|https://edo.tzstats.com|] -- this will produce a Nothing value,
   NamedChain_Edo2net -> [uri|https://edo.tzstats.com|] -- this will produce a Nothing value,
   NamedChain_Florencenet -> [uri|https://florence.tzstats.com|] -- this will produce a Nothing value,
-  NamedChain_Granadanet -> [uri|https://granada.tzstats.com/|]
+  NamedChain_Granadanet -> [uri|https://granada.tzstats.com/|] -- this will produce a Nothing value
   -- update this when this URL becomes available
 
 blockExplorerLink :: (MonadReader r m, HasFrontendConfig r, DomBuilder t m, PostBuild t m) => Dynamic t Text -> m a -> m a
