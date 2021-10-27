b2e09e67933aec26462ecd92fad708e1a389ca88
Roman Melnikov <roman.melnikov@serokell.io>
2021-10-27 19:04:47 +0300

    [#81] Use v10.3 Tezos binaries
    Problem: Tezos recently had a new v10.3 release that introduces some bug
    fixes, Kiln should use binaries with these fixes.
    
    Solution: Use v10.3 binaries in Kiln.

--- dep/serokell-tezos-binaries/default.nix
+++ dep/serokell-tezos-binaries/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "tezos-${version}";
-  version = "10.2";
+  version = "10.3";
 
   src = builtins.fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/v${version}-1/binaries-${version}-1.tar.gz";
-      sha256 = "04hmhmjj4782n85avr6wxcpv01li62m8h6qw7bmbbysqi59ww5zl";
+      sha256 = "087wfar7g7jrrqa63wb6g5nz64v5nz710dywqm49piyml9ymj10v";
       };
   binaries = ["tezos-client" "tezos-node" "tezos-baker-*" "tezos-endorser-*" "tezos-admin-client"];
   installPhase = ''
