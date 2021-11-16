bfbc1909a6a10d435c898f045b0b46bd56badb41
Roman Melnikov <roman.melnikov@serokell.io>
2021-11-16 14:35:54 +0300

    Use Tezos v11.0 binaries
    Problem: Tezos recently announced a first stable 11.0 release.
    
    Solution: Use binaries from the latest release in Kiln.

--- dep/brew-tezos-binaries/default.nix
+++ dep/brew-tezos-binaries/default.nix
@@ -1,35 +1,35 @@
 { pkgs }:
 with pkgs;
 let
-  outer-version = "v11.0-rc2-1";
+  outer-version = "v11.0-1";
   macos_version = "mojave";
     tezos-admin-client = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-admin-client-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "0hpqq2wa07pdhhiv2dkn55p6i2wysdcy406drshmk3x2il1zv2c5";
+      sha256 = "1r88kl563zqjywg5v5q99jiavjrsx3nlmkj3xr75m9spx3ac07kj";
     };
     tezos-baker-010-PtGRANAD = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-baker-010-PtGRANAD-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "10a6bss34hqm8cr6z4dhhzqps9vzqi6606pfklybakfkhz3smkmj";
+      sha256 = "1r5wpgvf8ki5223f9dh039n5cgqn3a60s545i13pbv1ibfrs7p26";
     };
     tezos-baker-011-PtHangz2 = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-baker-011-PtHangz2-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "1d8iiyx24asx2dbir4s25zxny8pya2zdqhgpzysg4khlh0md56zh";
+      sha256 = "01yw13pd8036kp97n2wv8z1kq6i03mj7fdwkk1395r2z936v9qpi";
     };
     tezos-client = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-client-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "0nlsynizc65fx5i1i9ca7amq33wwfizvhh1gppj1aypzwdsd32c5";
+      sha256 = "1i89dndk7qlcclxb84hif911zlddb8la8arswwp3v74ba4chhmsm";
     };
     tezos-endorser-010-PtGRANAD = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-endorser-010-PtGRANAD-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "0zrd4craxlsav2m917kp2zif2vvjcbj82rv7imjrjrlpxziz8ff5";
+      sha256 = "183rmvr6723xd8q0swjyn1h4bmphla3njgfm2cwzbfniqmm76dq7";
     };
     tezos-endorser-011-PtHangz2 = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-endorser-011-PtHangz2-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "154lbyg0x9sfg4wxq16ndp127r7r1v0cag5xm9h4xfqlasvw093i";
+      sha256 = "01xz7z06qsfy2a8c0zq8alxqr57dh49ml32a9q8s7ik0vy5bj0wp";
     };
     tezos-node = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-node-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "1hx6cqfm9ap51ca0z131x5l0ilqhprg6ma9zw1gbf36kmqi3prn1";
+      sha256 = "1c259igmljydi1x9azph3kl55v94bwr3vjr351p6185gj6506151";
     };
 
 in stdenv.mkDerivation rec {
