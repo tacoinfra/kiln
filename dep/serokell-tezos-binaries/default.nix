59651cba2ab40740deb3ce6ea528ac7822ef36e9
DMozhevitin <RayZieger@yandex.ru>
2022-05-10 17:16:46 +0300

    [#133] Use Octez v13.0 binaries
    Problem: A new v13.0 Octez version was released, we need
    to use these binaries in Kiln as well.
    
    Solution: Use v13.0 Octez binaries in Kiln.

--- dep/serokell-tezos-binaries/default.nix
+++ dep/serokell-tezos-binaries/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "tezos-${version}";
-  version = "13.0-rc1-2";
+  version = "13.0-1";
 
   src = fetchzip {
       url = "https://github.com/serokell/tezos-packaging/releases/download/v${version}/binaries-${version}.tar.gz";
-      sha256 = "0fdbjkn3dhdcj0rnbr7jkpjijzg1mzvrdznwcx1jyvd7njyflpwz";
+      sha256 = "1x03qfz0vjvfbfnzby8bsnqsf8r1fg7y2mf0szaadngxz2sfwl95";
       stripRoot = false;
       };
   binaries = ["tezos-client" "tezos-node" "tezos-baker-*" "tezos-endorser-*" "tezos-admin-client"];
