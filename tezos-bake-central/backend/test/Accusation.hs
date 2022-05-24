a907a522d5746610c0feda82bdc6dcf1fa770b64
DMozhevitin <RayZieger@yandex.ru>
2022-05-24 23:16:08 +0300

    [#124] Remove old accusations tests
    Problem: There are some tests for accusations related to
    Hangzhout protocol, but this protocol isn't supported by
    'tezos-bake-monitor-lib'.
    
    Solution: Remove accusations tests for Hangzhou.

--- tezos-bake-central/backend/test/Accusation.hs
+++ tezos-bake-central/backend/test/Accusation.hs
@@ -12,18 +12,13 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import Backend.Workers.Block (getAccusedBaker)
-import qualified Tezos.V011.Types as V011
 import Tezos.V012.Types
 import qualified Tezos.V012.Types as V012
 
 
 testAccusations :: TestTree
 testAccusations = testGroup "Accusations"
-  [ testGroup "011"
-    [ testDoubleBakingEvidence011
-    , testDoubleEndorsementEvidence011
-    ]
-  , testGroup "012"
+  [ testGroup "012"
     [ testDoubleBakingEvidence012
     , testDoublePreendorsementEvidence012
     ]
@@ -43,22 +38,6 @@ baseAccusationTest testName getBalanceUpdates path expected = testCase testName 
     balanceUpdates = getBalanceUpdates op
     accusedBaker = getAccusedBaker balanceUpdates
   accusedBaker @?= expected
-
-testDoubleBakingEvidence011 :: TestTree
-testDoubleBakingEvidence011 = baseAccusationTest
-  "Double baking evidence 011"
-  (toList . V011._doubleBakingEvidenceMetadata_balanceUpdates . V011._operationContentsDoubleBakingEvidence_metadata)
-  -- https://hangzhounet.tzkt.io/opN53nP6bdWffc28GeeEnGxK5NfpWMx26w84EZAKf2Uy3vQkjCd
-  "test/Accusations/011/double_baking_evidence.json"
-  "tz1SMARcpWCydHsGgz4MRoK9NkbpBmmUAfNe"
-
-testDoubleEndorsementEvidence011 :: TestTree
-testDoubleEndorsementEvidence011 = baseAccusationTest
-  "Double endorsement evidence 011"
-  (toList . V011._doubleEndorsementEvidenceMetadata_balanceUpdates . V011._operationContentsDoubleEndorsementEvidence_metadata)
-  -- https://hangzhounet.tzkt.io/oodotr3y1CH1GdkvxBTuiJqFoApM7tTZRmMCmQkMeFWdK4E94Yk
-  "test/Accusations/011/double_endorsement_evidence.json"
-  "tz1aWXP237BLwNHJcCD4b3DutCevhqq2T1Z9"
 
 testDoubleBakingEvidence012 :: TestTree
 testDoubleBakingEvidence012 = baseAccusationTest
