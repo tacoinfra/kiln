607368371e0c9332c6c723b20c24aa7937b67103
Roman Melnikov <roman.melnikov@serokell.io>
2022-06-15 16:41:54 +0300

    [#143] Support RPC endpoint for getting address balance
    Problem: We need a way to reliably get address balance without
    calling 'tezos-client'.
    
    Solution: Expose new node RPC enpdoint provided by
    'tezos-bake-monitor-lib'.

--- tezos-bake-central/backend/src/Backend/NodeRPC.hs
+++ tezos-bake-central/backend/src/Backend/NodeRPC.hs
@@ -149,6 +149,7 @@ data NodeQuery a where
   NodeQuery_BlockHeader       :: BlockHash -> NodeQuery BlockHeader
   NodeQuery_DelegateInfo      :: BlockHash -> RawLevel -> PublicKeyHash -> NodeQuery CacheDelegateInfo
   NodeQuery_ParticipationInfo :: BlockHash -> RawLevel -> PublicKeyHash -> NodeQuery ParticipationInfo
+  NodeQuery_Balance           :: BlockHash -> RawLevel -> PublicKeyHash -> NodeQuery Tez
   NodeQuery_Blocks            :: BlockHash -> RawLevel -> NodeQuery (Seq BlockHash)
   NodeQuery_Round             :: BlockHash -> NodeQuery Int32
 deriving instance Show (NodeQuery a)
@@ -601,6 +602,7 @@ getContext = \case
   NodeQuery_CurrentQuorum ctx -> pure ctx
   NodeQuery_DelegateInfo ctx _lvl _pkh -> pure ctx
   NodeQuery_ParticipationInfo ctx _lvl _pkh -> pure ctx
+  NodeQuery_Balance ctx _lvl _pkh -> pure ctx
   NodeQuery_Blocks ctx _ -> pure ctx
   NodeQuery_Round ctx -> pure ctx
 
@@ -793,6 +795,7 @@ nodeQueryImpl doNodeRPC toChain chainId ctx logger q = runExceptT $ runLoggingEn
   NodeQuery_BlockHeader branch -> nodeRPC' $ rBlockHeader (toChain chainId) branch
   NodeQuery_DelegateInfo branch _lvl pkh -> fmap (fmap toCacheDelegateInfo) $ nodeRPC' $ rDelegateInfo pkh chainId branch
   NodeQuery_ParticipationInfo branch _lvl pkh -> nodeRPC' $ rParticipationInfo pkh chainId branch
+  NodeQuery_Balance branch _lvl pkh -> nodeRPC' $ rBalance pkh chainId branch
   NodeQuery_Blocks branch length' -> do
     (RpcResult _ response) <- nodeRPC' $ rBlocks chainId length' (Set.singleton branch)
     let blocks = branch Seq.<| fromMaybe mempty (Map.lookup branch response)
