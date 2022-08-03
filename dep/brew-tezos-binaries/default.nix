49e92be3ed7ef886f82365ddf9e62eea0e7310d6
dmitriy.mozhevitin <dmitriy.mozhevitin@serokell.io>
2022-08-03 16:28:04 +0300

    [#162] Use Octez v14.0 binaries
    Problem: A new v14.0 Octez version has been released, we need
    to use these binaries in Kiln as well.
    
    Solution: Use v14.0 Octez binaries in Kiln. Also switch from
    'catalina' bottles to 'big_sur' because 'tezos-packaging' doesn't
    support 'catalina' now.

--- dep/brew-tezos-binaries/default.nix
+++ dep/brew-tezos-binaries/default.nix
@@ -1,27 +1,27 @@
 { pkgs }:
 with pkgs;
 let
-  outer-version = "v13.0-1";
-  macos_version = "catalina";
+  outer-version = "v14.0-1";
+  macos_version = "big_sur";
     tezos-admin-client = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-admin-client-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "187pcwah69lgpq0ljjcfhs1phnfdndg7l991h9w8na1q84bj3vbc";
+      sha256 = "1qlcxb20v2nbycd8z95bp7ybd4qqh5ik5wfw2anrd87jy7f7mnbn";
     };
     tezos-baker-013-PtJakart = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-baker-013-PtJakart-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "0b9gj7qfvhcqi2ca56kz979q34iy7k1ls13m5zzh8fjqdq907lqw";
+      sha256 = "0v0mw9lag75qb8kfwdz0kmq8y5dq4fxpljlidkjzi4a6f718djzp";
     };
-    tezos-baker-012-Psithaca = fetchTarball {
-      url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-baker-012-Psithaca-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "0mw39i2jrcmdr3fqzr86hy7mmc3y9s96sqi52xh2jvvmzs9d07ip";
+    tezos-baker-014-PtKathma = fetchTarball {
+      url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-baker-014-PtKathma-${outer-version}.${macos_version}.bottle.tar.gz";
+      sha256 = "1cpwqk3h5m4spxfgrijr6xk8jiqy821hnp63plqixzfrgy5a8d15";
     };
     tezos-client = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-client-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "1rjwimk15v8fgqr9dnb7r64k03rac48sdaaamrv1r115lndwaphm";
+      sha256 = "1shqlwv2gwki04h0k5nbfqiblga7zm31ixpgyw44cljaw542shid";
     };
     tezos-node = fetchTarball {
       url = "https://github.com/serokell/tezos-packaging/releases/download/${outer-version}/tezos-node-${outer-version}.${macos_version}.bottle.tar.gz";
-      sha256 = "1s2jnfxwfy2a07sqap7k8fwl81akh1gmwf0fiqwshbp6hb9xlmjn";
+      sha256 = "1ss9hmha4i7n2zgsy65d5x34vj9fz61awa8x3ng3klprzbkxaf11";
     };
 
 in stdenv.mkDerivation rec {
@@ -38,8 +38,8 @@ in stdenv.mkDerivation rec {
   cp ${tezos-baker-013-PtJakart}/${outer-version}/bin/tezos-baker-013-PtJakart $out/bin/tezos-baker-013-PtJakart
   chmod +x $out/bin/tezos-baker-013-PtJakart
 
-  cp ${tezos-baker-012-Psithaca}/${outer-version}/bin/tezos-baker-012-Psithaca $out/bin/tezos-baker-012-Psithaca
-  chmod +x $out/bin/tezos-baker-012-Psithaca
+  cp ${tezos-baker-014-PtKathma}/${outer-version}/bin/tezos-baker-014-PtKathma $out/bin/tezos-baker-014-PtKathma
+  chmod +x $out/bin/tezos-baker-014-PtKathma
 
   cp ${tezos-client}/${outer-version}/bin/tezos-client $out/bin/tezos-client
   chmod +x $out/bin/tezos-client
