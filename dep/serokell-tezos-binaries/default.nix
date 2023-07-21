d104f3d0c174d5a7425ca0b6db70ad4fd17fb57b
dmitriy.mozhevitin <dmitriy.mozhevitin@serokell.io>
2023-07-21 14:03:04 +0300

    [Chore] Use v17.2 Octez binaries
    Problem: The new version of Octez binaries
    has been released, and we need to use these
    binaries in Kiln as well.
    
    Solution: Use v17.2 Octez binaries in Kiln.

--- dep/serokell-tezos-binaries/default.nix
+++ dep/serokell-tezos-binaries/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "tezos-${version}";
-  version = "17.1-1";
+  version = "17.2-1";
 
   src = fetchzip {
       url = "https://github.com/serokell/tezos-packaging/releases/download/v${version}/binaries-${version}.tar.gz";
-      sha256 = "sha256-5+CLlBTSuHfVnwxDx1yAVT5KwJR6VVHyJjqfjAzsPxg=";
+      sha256 = "sha256-++f6m6TMJfNgFMarVTCquyl4zW9VSqvIy+EIj+he2C8=";
       stripRoot = false;
       };
   binaries = ["octez-client" "octez-node" "octez-baker-*"];
