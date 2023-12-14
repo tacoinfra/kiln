cd6f055f9cd21309ef2f457ff5618a95a99ee356
dmitriy.mozhevitin <dmitriy.mozhevitin@serokell.io>
2023-12-14 14:56:55 +0300

    [Chore] Bump Kiln version

--- default.nix
+++ default.nix
@@ -112,7 +112,7 @@ in (obApp false distroMethods.source system) // {
     obApp = obAppGargoyle false distroMethods.linuxPackage "x86_64-linux";
     nodeKit = tezosScopedKit false "x86_64-linux";
     pkgName = "kiln";
-    version = "0.17.2"; # TODO: Calculate this
+    version = "0.18.0"; # TODO: Calculate this
     inherit zcash;
   }).kiln-debian;
 
