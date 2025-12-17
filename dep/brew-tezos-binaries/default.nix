024e5065ebe0975e077f68e900979850e42de62b
Heitor Toledo Lassarote de Paula <heitor.toledo@serokell.io>
2025-12-17 11:08:44 -0300

    [#248] Use 24.0-rc1 for Tallinn protocol
    Problem: Octez 24.0-rc1 or newer is needed for Tallinn protocol.
    
    Solution: Update Octez versions, URLs, and their hashes to point to the
    new version.

--- dep/brew-tezos-binaries/default.nix
+++ dep/brew-tezos-binaries/default.nix
@@ -1,14 +1,14 @@
 { pkgs }:
 with pkgs;
 let
-  outer-version = "23.2";
+  outer-version = "24.0-rc1";
 
 in stdenv.mkDerivation rec {
   name = "tezos-${outer-version}";
   src = builtins.fetchTarball {
-    # Take from https://gitlab.com/tezos-kiln/kiln/-/packages/45953303
-    url = "https://gitlab.com/tezos-kiln/kiln/-/package_files/230108484/download";
-    sha256 = "08sdn48bhjl3zhi0lh3wxw5ga19lqxqxb658c5jy76wpr4ms26ib";
+    # Taken from https://gitlab.com/tezos-kiln/kiln/-/packages/50312361
+    url = "https://gitlab.com/tezos-kiln/kiln/-/package_files/254003847/download";
+    sha256 = "0fhqmrlig2vg8xh8n9acnpni42cgkv6rwh40sjkb1xvsb3jhyalh";
   };
 
   phases = [ "installPhase" ];
